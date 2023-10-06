f43bd55189555b688e2bef7b5f1c827811c57efb
uzo-john <johnpauluzowuru2018@gmail.com>
2023-10-06 12:38:52 -0700

    task done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a method that determines if all the boxes can be opened.
-"""
-
-
+"""Write a method that determines if all the boxes can be opened."""
 
 def canUnlockAll(boxes):
     n = len(boxes)
